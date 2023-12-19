ec3724019248d63217f2a2af3b8c75cdc3da013e
vanhuutoan27 <vhtoan27@gmail.com>
2023-12-19 22:59:01 +0700

    fix bugs deploy vercel

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,18 +1,16 @@
 import { Routes, Route } from 'react-router-dom';
 
 import Home from './pages/Home/Home';
-import Product from './pages'
+import Product from './pages/Product/Product';
 
 import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/product" element={<Product />} />
-      </Routes>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/product" element={<Product />} />
+    </Routes>
   );
 }
 
